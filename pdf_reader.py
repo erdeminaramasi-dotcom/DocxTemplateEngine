ac5c84a9e61b8d188c525987613b81cfac8efff5
erdeminaramasi-dotcom <erdeminaramasi@gmail.com>
2026-07-27 02:14:27 +0300

    Create pdf_reader.py

---
+++ pdf_reader.py
@@ -0,0 +1,30 @@
+import fitz
+
+
+class PDFReader:
+
+    def __init__(self, pdf_path):
+        self.pdf = fitz.open(pdf_path)
+
+    def read(self):
+
+        pages = []
+
+        for page in self.pdf:
+
+            pages.append(page.get_text())
+
+        return pages
+
+
+if __name__ == "__main__":
+
+    reader = PDFReader("../samples/sample.pdf")
+
+    pages = reader.read()
+
+    for i, page in enumerate(pages):
+        print("="*50)
+        print("PAGE", i+1)
+        print("="*50)
+        print(page)
